7e7e1b801f5e2e02f93a1ce25e527c88a1b7fd27
yukaary <yukaary.craft@gmail.com>
2014-08-07 22:15:14 +0900

    * modify command format.

--- VoiceConsoroid/Program.cs
+++ VoiceConsoroid/Program.cs
@@ -19,7 +19,7 @@ namespace VoiceConsoroid
     {
         static void Main(string[] args)
         {
-            if (args.Length < 4)
+            if (args.Length < 3)
             {
                 Console.WriteLine("few arguments.");
                 Console.WriteLine("");
@@ -39,7 +39,7 @@ namespace VoiceConsoroid
                 return;
             }
 
-            Voiceroid voiceroid = find(argsMap["-v"]);
+            Voiceroid voiceroid = find(argsMap["voice"]);
             if (voiceroid == null)
             {
                 Console.WriteLine("Can't find voiceroid process {0}.", argsMap["-v"]);
@@ -112,15 +112,15 @@ namespace VoiceConsoroid
         private static void PrintUsage()
         {
             Console.WriteLine("Usage:");
-            Console.WriteLine("    VoiceConsoroid <command> <message> -v <voiceroid> -f <filepath>");
+            Console.WriteLine("    VoiceConsoroid <voiceroid> <command> <message> -f <filepath>");
             Console.WriteLine("");
             Console.WriteLine("    Parameters");
+            Console.WriteLine("        (required) voiceroid: specify voideroid. set yukari or maki");
             Console.WriteLine("        (required) command:");
             Console.WriteLine("            talk   : play message on GUI.");
             Console.WriteLine("            save   : save wav file into specified file path.");
             Console.WriteLine("            record : save wav file into specified file path, then play that wav.");
             Console.WriteLine("        (required) message: message should not be empty.");
-            Console.WriteLine("        (required) -v: specify voideroid. set yukari or maki");
             Console.WriteLine("          (option) -f: specify file path to save wav. default is voice.wav.");
         }
 
@@ -129,26 +129,21 @@ namespace VoiceConsoroid
             Dictionary<string, string> argsMap = new Dictionary<string, string>();
 
             // Initialize disctionary
+            argsMap["voice"] = "";
             argsMap["command"] = "";
             argsMap["message"] = "";
-            argsMap["-v"] = "";
             argsMap["-f"] = "";
 
-            // 4 arguments are required at least.
-            if(args.Length < 4)
-            {
-                return argsMap;
-            }
-
+            // store voiceroid
+            argsMap["voice"] = args[0];
             // store main command.
-            argsMap["command"] = args[0];
+            argsMap["command"] = args[1];
             // store message.
-            argsMap["message"] = args[1];
+            argsMap["message"] = args[2];
 
             // store options
-            for(int i = 2; i < args.Length; i+=2)
+            for(int i = 3; i < args.Length; i+=2)
             {
-                //argsMap.Add(args[i], args[i + 1]);
                 argsMap[args[i]] = args[i + 1];
             }
 
